c9e6d397efeea9eb1ee63dd8022fd79f897fba87
Tee O <tdopress@gmail.com>
2023-05-17 12:54:36 -0400

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+//connects our react application to the index.html file
+ReactDOM.render(< App/>, document.getElementById('root'))
