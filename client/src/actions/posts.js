326510a8587ae8ddfc0c3c55b6e4546b6dfd0a54
Tee O <tdopress@gmail.com>
2023-05-22 17:45:44 -0400

    implement create post action

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -10,3 +10,13 @@ export const getPosts = () => async (dispatch) => {
         
     }
 }
+export const createPost = (post) => async (dispatch) => {
+    try {
+        console.log('Create Post Action', post)
+        const { data } = await api.createPost(post);
+        console.log(data)
+        dispatch({ type: 'CREATE', payload: data })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
