5973f6bbf34c89e0175e6bb2a42cbe7af41ce77f
Tee O <tdopress@gmail.com>
2023-05-23 17:52:17 -0400

    implement functionality to update post in database

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,6 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
 import PostMessage from '../models/PostMessage.js'
 export const getPosts = async (req, res) => {
     try {
@@ -9,7 +12,7 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPosts = async (req, res) => {
+export const createPost = async (req, res) => {
     const post = req.body
     const newPost = new PostMessage(post)
     try {
@@ -20,3 +23,22 @@ export const createPosts = async (req, res) => {
         res.status(409).json({message: error.message})
     }
 }
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params
+    const post = req.body
+    
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID.')
+    
+    try {
+        //new option allows returns updated post
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+        console.log(updatedPost)
+
+        res.status(200).json(updatedPost)
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({ message: error.message })
+    }
+
+} 
