0340c5b43fff58ed3611b52b538567e38fa41ffc
Tee O <tdopress@gmail.com>
2023-05-23 15:46:11 -0400

    create posts grid

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid, CircularProgress } from '@material-ui/core'
 import Post from './Post/Post'
 import useStyles from './styles'
 import { useSelector } from 'react-redux'
@@ -7,12 +8,15 @@ const Posts = () => {
   const classes = useStyles()
   const posts = useSelector(state => state.posts)
   console.log(posts)
-  return (
-    <div>
-      <h1>Posts</h1>
-        <Post />
-        <Post />
-    </div>
+  return ( 
+    !posts.length ? <CircularProgress/> :
+      <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+        {posts.map((post) => 
+          (<Grid key={post._id} item xs={12} sm={6} md={6} lg={3}>
+            <Post post={post} />
+          </Grid>) 
+        )}
+      </Grid>
   )
 }
 
