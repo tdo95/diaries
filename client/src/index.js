f9d0b44fbab2bc3c8972cb5c95984ed269639512
Tee O <tdopress@gmail.com>
2023-05-23 15:46:24 -0400

    add background svg

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk'
 import reducers from './reducers'
 
 import App from './App'
-
+import './index.css'
 //thunk allows us to perform asynchronous actions in redux
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
