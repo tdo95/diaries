7db7b114e5a58f77cc50db854d7d1846c033ba25
Tee O <tdopress@gmail.com>
2023-05-23 15:45:57 -0400

    add card for individual post

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box } from '@material-ui/core'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import moment from 'moment'
+
 import useStyles from './styles'
 
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles()
   return (
-    <div>Post</div>
+    <Card className={classes.card} raised elevation={6}>
+      <CardMedia className={classes.media} image={post.selectedFile.base64} title={post.title}/>
+      <div className={classes.overlay}>
+        <Typography variant='h6'>{post.creator}</Typography>
+        <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div className={classes.overlay2}>
+          <Button style={{color: 'white'}} size='small' onClick={() => {}}>
+            <MoreHorizIcon fontSize='medium'/>
+          </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant='body2' color="textSecondary">{post.tags.map(tag => `#${tag} `)}</Typography>
+      </div>
+      <CardContent>
+      <Typography className={classes.title} variant='h5' gutterBottom>{post.title}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size='small' style={{color: 'black'}} onClick={() => {}}>
+          <FavoriteBorderIcon fontSize='small'/>
+            Like 
+            {post.likeCount}
+        </Button>
+        <Button size='small' style={{color: 'black'}} onClick={() => {}}>
+          <DeleteOutlineIcon fontSize='small'/>
+            Delete
+        </Button>
+      </CardActions>
+      
+
+
+    </Card>
   )
 }
 
