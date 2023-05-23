7fe7520371ef8add5848b665239a05360c4bb688
Tee O <tdopress@gmail.com>
2023-05-23 17:50:16 -0400

    add functionality to update post on sumbit and clear form

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,12 +1,13 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import { Typography, TextField, Button, Paper, Box } from '@material-ui/core'
 import FileBase from 'react-file-base64'
-import { useDispatch } from 'react-redux'
-import { createPost } from '../../actions/posts'
+import { useDispatch, useSelector } from 'react-redux'
+import { createPost, updatePost } from '../../actions/posts'
 
 import useStyles from './styles'
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
+  const post = useSelector((state) => currentId ? state.posts.find(p => p._id === currentId) : null)
   const [postData, setPostData] = useState({
     creator: '',
     title: '',
@@ -16,17 +17,27 @@ const Form = () => {
   })
   const classes = useStyles()
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (post) setPostData(post)
+  }, [post])
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(postData)
-    dispatch(createPost(postData))
     
+    if (currentId) dispatch(updatePost(currentId, postData))
+    else dispatch(createPost(postData))
+
+    clear()
+  }
+  const clear = () => {
+    setCurrentId(null)
+    setPostData({creator: '', title: '', message: '', tags: '', selectedFile: '',})
   }
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant='h6'>Create a Post</Typography>
+        <Typography variant='h6'>{currentId ? "Edit" : "Create"} Post</Typography>
         <TextField
           name='creator'
           variant='outlined'
