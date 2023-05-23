8a88342589b5fa47bcada284bb91f89aa6a78470
Tee O <tdopress@gmail.com>
2023-05-23 17:49:14 -0400

    implement action to update post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -6,17 +6,24 @@ export const getPosts = () => async (dispatch) => {
         const { data } = await api.fetchPosts()
         dispatch({ type: 'FETCH_ALL', payload: data })
     } catch (error) {
-        console.log(error.message)
+        console.log(error)
         
     }
 }
 export const createPost = (post) => async (dispatch) => {
     try {
-        console.log('Create Post Action', post)
         const { data } = await api.createPost(post);
-        console.log(data)
         dispatch({ type: 'CREATE', payload: data })
     } catch (error) {
-        console.log(error.message)
+        console.log(error)
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post)
+        dispatch({ type: 'UPDATE', payload: data })
+    } catch (error) {
+        console.log(error)
     }
 }
