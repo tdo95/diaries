8469fbbd34754e80c9179bd8e4dba29a03ac5556
Tee O <tdopress@gmail.com>
2023-05-23 17:48:35 -0400

    add currentId variable to track selected post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react'
+import { React, useEffect, useState } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 import { getPosts } from './actions/posts'
@@ -11,11 +11,13 @@ import useStyles from './styles'
 
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(null)
   const classes = useStyles()
   const dispatch = useDispatch()
   useEffect(() => {
+    console.log('getting list of posts')
     dispatch(getPosts())
-  }, [])
+  }, [currentId, dispatch])
 
   return (
     <Container maxwidth='lg'>
@@ -27,10 +29,10 @@ const App = () => {
         <Container>
           <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId}/>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId}/>
             </Grid>
           </Grid>
         </Container>
