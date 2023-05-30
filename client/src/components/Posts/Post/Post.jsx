7bd396541687a8258a38741c7099d4311cc2b24d
Tee O <tdopress@gmail.com>
2023-05-30 15:46:43 -0400

    add functionality to delete post on click

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -7,9 +7,12 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 
 import useStyles from './styles'
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles()
+  const dispatch = useDispatch()
   return (
     <Card className={classes.card} raised elevation={6}>
       <CardMedia className={classes.media} image={post.selectedFile.base64} title={post.title}/>
@@ -34,7 +37,7 @@ const Post = ({ post, setCurrentId }) => {
             Like 
             {post.likeCount}
         </Button>
-        <Button size='small' style={{color: 'black'}} onClick={() => {}}>
+        <Button size='small' style={{color: 'black'}} onClick={() => dispatch(deletePost(post._id))}>
           <DeleteOutlineIcon fontSize='small'/>
             Delete
         </Button>
