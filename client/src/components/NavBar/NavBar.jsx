f048fe9a95c43cf1dbba63f35494f6624afe795a
Tee O <tdopress@gmail.com>
2023-06-03 13:46:17 -0400

    add sign in button and home link to nav

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
-import { AppBar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import { AppBar, Typography, Avatar, Toolbar, Button } from '@material-ui/core'
 
 import logo from '../../images/logo.png'
 import useStyles from './styles'
 
 const NavBar = () => {
     const classes = useStyles()
+    const user = null
   return (
     <AppBar className={classes.appBar} position='static' color='inherit'>
-        <img className={classes.image} src={logo} alt='diaries' height='60'/>
-        <Typography className={classes.heading} variant='h2' align='center'>Diaries</Typography>
+        <Link to='/' className={classes.link}>
+          <div  className={classes.brandContainer}>
+            <img className={classes.image} src={logo} alt='diaries' height='60'/>
+            <Typography className={classes.heading} variant='h2' align='center'>Diaries</Typography>
+          </div>
+        </Link>
+        <Toolbar className={classes.toolbar}>
+          {user ? (
+              <div className={classes.profile}>
+                <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name[0]}</Avatar>
+                <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+                <Button className={classes.logout} color="secondary" onClick={() => {}} variant='contained'>Log out</Button>
+              </div>
+
+            ) : (
+              <Button component={Link} to='/auth' className={classes.logout} variant='contained'>Sign In</Button>
+
+            )}
+        </Toolbar>
     </AppBar>
   )
 }
