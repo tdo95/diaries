0bbb9ac2f083c7b524cadb6ee05f4c6b999383fd
Tee O <tdopress@gmail.com>
2023-06-03 11:49:28 -0400

    abstract main application page into home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,16 @@
-import { React, useEffect, useState } from 'react'
-import { Container, Typography, Grow, Grid } from '@material-ui/core'
-import { useDispatch } from 'react-redux'
-import { getPosts } from './actions/posts'
+import { React } from 'react'
+import { Container } from '@material-ui/core'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Form from './components/Form/Form'
-import Posts from './components/Posts/Posts'
 import NavBar from './components/NavBar/NavBar'
-
-import useStyles from './styles'
-
+import Home from './components/Home/Home'
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null)
-  const classes = useStyles()
-  const dispatch = useDispatch()
-  useEffect(() => {
-    console.log('getting list of posts')
-    dispatch(getPosts())
-  }, [currentId, dispatch])
 
   return (
     <Container maxwidth='lg'>
       <NavBar/>
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justifyContent='space-between' alignItems='stretch' spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId}/>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Home />
     </Container>
   )
 }
