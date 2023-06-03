f2f0c5b8668ccf602b3cee7eb029be989f510a0d
Tee O <tdopress@gmail.com>
2023-06-03 13:46:46 -0400

    add styling for nav components

--- client/src/components/NavBar/styles.js
+++ client/src/components/NavBar/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
 
 export default makeStyles((theme) => ({
     appBar: {
@@ -6,8 +7,9 @@ export default makeStyles((theme) => ({
         margin: '30px 0',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        padding: '10px 50px',
       },
       heading: {
         color: 'rgba(0,0,0, 1)',
@@ -17,4 +19,33 @@ export default makeStyles((theme) => ({
         borderRadius: '50px',
         marginRight: '15px'
       },
+      toolbar: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '400px',
+      },
+      profile: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '400px',
+      },
+      userName: {
+        display: 'flex',
+        alignItems: 'center',
+      },
+      brandContainer: {
+        display: 'flex',
+        alignItems: 'center',
+      },
+      purple: {
+        color: theme.palette.getContrastText(grey[900]),
+        backgroundColor: grey[900],
+      },
+      link: {
+        textDecoration: 'none'
+      },
+      logout: {
+        color: 'white',
+        backgroundColor: grey[900],
+      }
 }))
